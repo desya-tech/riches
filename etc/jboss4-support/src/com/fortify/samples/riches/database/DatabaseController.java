c4513d2aa1222e74dba7a82ba210a55f9d6c877d
desya.kristianto <desya.tech@gmail.com>
2023-07-08 01:30:09 +0700

    update

---
+++ etc/jboss4-support/src/com/fortify/samples/riches/database/DatabaseController.java
@@ -0,0 +1,31 @@
+/***********************************************************************
+ * (C) Copyright 2005  Fortify Software Inc.   All Rights Reserved.
+ *
+ *  Copyright 2005 Fortify Software, Inc. All rights reserved.
+ *  This software is provided solely in connection with the terms
+ *  of the license agreement.  Any other use without the prior express
+ *  written permission of Fortify Software is completely prohibited.
+ ************************************************************************/
+
+package com.fortify.samples.riches.database;
+
+import javax.servlet.ServletContextListener;
+import javax.servlet.ServletContextEvent;
+/* $File: //depot/releases/twopi/20.2.0_/samples/Samples/advanced/riches.java/riches/etc/jboss4-support/src/com/fortify/samples/riches/database/DatabaseController.java $ 
+ * Created by: adam on Aug 8, 2005
+ *
+ * Details of last change:
+ * $Author: adam.chesney $
+ * $DateTime: 2020/09/28 10:11:29 $
+ * $Revision: #1 $
+ * $Change: 250292 $
+ */
+
+public class DatabaseController implements ServletContextListener {
+
+	// do nothing, the functionality normally provided in this class is handled by JBoss MBeans
+
+	public void contextInitialized(ServletContextEvent servletContextEvent) { }
+	public void contextDestroyed(ServletContextEvent servletContextEvent) {	}
+}
+
